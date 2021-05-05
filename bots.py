0ce7f5d1714659b78fac7e7cc0af45818afc9b93
shookies <34584158+shookies@users.noreply.github.com>
2021-05-05 19:35:44 +0300

    Add files via upload
    start of RL agent

--- bots.py
+++ bots.py
@@ -6,6 +6,128 @@ import numpy as np
 import random
 
 
+class rlAgent(base_agent.BaseAgent):
+
+    base_top_left = None
+    actions = ("do_nothing",
+               "harvest_minerals",
+               "build_pylon",
+               "build_gateway",
+               "train_zealot",
+               "attack")
+
+    def step(self,obs):
+        super(rlAgent, self).step(obs)
+
+        if obs.first():
+            nexus = self.get_my_units_by_type(obs, units.Protoss.Nexus)[0]
+            self.base_top_left = (nexus.x < 32)
+        pylons = self.get_my_completed_units_by_type(obs, units.Protoss.Pylon)
+        gateways = self.get_my_completed_units_by_type(obs, units.Protoss.Gateway)
+        if len(gateways) > 0:
+            return self.train_zealots(obs)
+        if len(pylons) > 0:
+            return self.build_gateway(obs)
+        else:
+            return self.build_pylon(obs)
+        # return actions.RAW_FUNCTIONS.no_op()
+
+
+    #-----------------UTILITY----------------
+
+    def get_my_units_by_type(self, obs, unit_type):
+
+        return [unit for unit in obs.observation.raw_units if unit.unit_type == unit_type and
+                unit.alliance == features.PlayerRelative.SELF]
+
+    def get_my_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type and
+                unit.build_progress == 100 and
+                unit.alliance == features.PlayerRelative.SELF]
+
+    def get_enemy_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_enemy_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.build_progress == 100
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_distances(self, obs, units, xy):
+        units_xy = [(unit.x, unit.y) for unit in units]
+        return np.linalg.norm(np.array(units_xy) - np.array(xy), axis=1)
+
+
+    #-----------------ACTIONS----------------
+
+    def do_nothing(self, obs):
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def harvest_minerals(self, obs):
+
+        probes = self.get_my_units_by_type(obs, units.Protoss.Probe)
+        idle_probes = [probe for probe in probes if probe.order_length == 0]
+        if idle_probes:
+            mineral_patches = [unit for unit in obs.observation.raw_units
+                         if unit.unit_type in [
+                           units.Neutral.BattleStationMineralField,
+                           units.Neutral.BattleStationMineralField750,
+                           units.Neutral.LabMineralField,
+                           units.Neutral.LabMineralField750,
+                           units.Neutral.MineralField,
+                           units.Neutral.MineralField750,
+                           units.Neutral.PurifierMineralField,
+                           units.Neutral.PurifierMineralField750,
+                           units.Neutral.PurifierRichMineralField,
+                           units.Neutral.PurifierRichMineralField750,
+                           units.Neutral.RichMineralField,
+                           units.Neutral.RichMineralField750
+                         ]]
+            probe = random.choice(idle_probes)
+            distances = self.get_distances(obs, mineral_patches, (probe.x, probe.y))
+            patch = mineral_patches[np.argmin(distances)]
+            return actions.RAW_FUNCTIONS.Harvest_Gather_unit("now", probe.tag, patch.tag)
+
+    def build_pylon(self, obs):
+
+        pylons = self.get_my_units_by_type(obs, units.Protoss.Pylon)
+        probes = self.get_my_units_by_type(obs, units.Protoss.Probe)
+        free_supply = obs.observation.player.food_cap - obs.observation.player.food_used
+
+        if len(pylons) == 0 and obs.observation.player.minerals >= 100 and probes:
+            pylon_xy = (22, 26) if self.base_top_left else (35, 42)
+            distances = self.get_distances(obs, probes, pylon_xy)
+            probey = probes[np.argmin(distances)]
+            return actions.RAW_FUNCTIONS.Build_Pylon_pt("now", probey.tag, pylon_xy)
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def build_gateway(self, obs):
+
+        completed_pylons = self.get_my_completed_units_by_type(obs, units.Protoss.Pylon)
+        gateways = self.get_my_completed_units_by_type(obs,units.Protoss.Gateway)
+        probes = self.get_my_units_by_type(obs, units.Protoss.Probe)
+        if (len(completed_pylons) > 0 and len(gateways) == 0 and obs.observation.player.minerals >= 150 and len(probes) > 0):
+            gateway_xy = (22, 21) if self.base_top_left else (35, 45)
+            distances = self.get_distances(obs,probes, gateway_xy)
+            probe = probes[np.argmin(distances)]
+            return actions.RAW_FUNCTIONS.Build_Gateway_pt("now", probe.tag, gateway_xy)
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def train_zealots(self, obs):
+
+        completed_gateways = self.get_my_completed_units_by_type(obs, units.Protoss.Gateway)
+        free_supply = obs.observation.player.food_cap - obs.observation.player.food_used
+        if len(completed_gateways) > 0 and obs.observation.player.minerals >= 100 and free_supply > 1:
+            gateway = self.get_my_units_by_type(obs, units.Protoss.Gateway)[0]
+            if gateway.order_length < 5:
+                return actions.RAW_FUNCTIONS.Train_Zealot_quick("now", gateway.tag)
+        return actions.RAW_FUNCTIONS.no_op()
+
+
 class ProtossAgent(base_agent.BaseAgent):
 
     def __init__(self):
@@ -81,11 +203,11 @@ class ProtossAgent(base_agent.BaseAgent):
 
         completed_gateways = self.get_my_completed_units_by_type(obs, units.Protoss.Gateway)
         free_supply = obs.observation.player.food_cap - obs.observation.player.food_used
-        if obs.observation.player.idle_worker_count > 0
-        if free_supply <= 2 and obs.observation.player.idle:
-            offset_x = random.randint(-4,4)
-            offset_y = random.randint(-4, 4)
-            return self.build_pylon(obs, (pylon_xy[0] + offset_x, pylon_xy[1] + offset_y))
+        # if obs.observation.player.idle_worker_count > 0
+        # if free_supply <= 2 and obs.observation.player.idle:
+        #     offset_x = random.randint(-4,4)
+        #     offset_y = random.randint(-4, 4)
+        #     return self.build_pylon(obs, (pylon_xy[0] + offset_x, pylon_xy[1] + offset_y))
 
         if len(completed_gateways) > 0 and obs.observation.player.minerals >= 100 and free_supply >= 2:
             gateway = gateways[0]
@@ -107,7 +229,7 @@ class ProtossAgent(base_agent.BaseAgent):
 
 
 def main(unused_argv):
-    agent = ProtossAgent()
+    agent = rlAgent()
     try:
         while True:
             with sc2_env.SC2Env(
